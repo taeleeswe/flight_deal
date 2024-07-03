72346c8ae1bc99fdeb9d0aad4490cdc36f60bc83
Tae Young Lee <taeleeswe@gmail.com>
2024-07-03 10:10:38 -0400

    second commit

--- flight_search.py
+++ flight_search.py
@@ -1,4 +1,25 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class FlightSearch:
+
+    def __init__(self):
+        self._api_key = os.environ["AMADEUS_API"]
+        self._api_secret = os.environ["AMADEUS_API_SECRET"]
+        self._token = self._get_new_token()
+
+    def _get_new_token(self):
+        header = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        body = {
+            "grant_type": 'client_credentials',
+            "client_id": self._api_key,
+            "client_secret": self._api_secret,
+        }
 
     def get_destination_code(self, city_name):
         code = "TESTING"
